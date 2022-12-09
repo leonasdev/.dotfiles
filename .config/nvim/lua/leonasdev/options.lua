5a9464c0305ba964ca01f40e813077a815304df5
leonasdev <s8952889@gmail.com>
2022-12-09 18:13:53 +0800

    set indent to 4 spaces... for better looking..?

--- .config/nvim/lua/leonasdev/options.lua
+++ .config/nvim/lua/leonasdev/options.lua
@@ -5,14 +5,18 @@ opt.relativenumber = true
 opt.number = true
 
 -- tabs & indentation
-opt.tabstop = 2
-opt.shiftwidth = 2
-opt.softtabstop = 2
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.softtabstop = 4
 opt.expandtab = true
 opt.autoindent = true
 
 -- line wrapping
 opt.wrap = false
+-- opt.wrap = true
+-- opt.breakindent = true
+-- opt.showbreak = string.rep(" ", 3) -- Make it so that long lines wrap smartly
+-- opt.linebreak = true
 
 --search & replace settings
 opt.ignorecase = true
@@ -51,6 +55,8 @@ opt.iskeyword:append("-") -- now 'test-test' is a word
 opt.scrolloff = 10
 opt.path:append { '**' } -- Finding files - Search down into subfolders
 opt.updatetime = 100 -- ref: https://www.reddit.com/r/vim/comments/jqogan/how_does_a_lower_updatetime_lead_to_better/
+opt.belloff = "all" -- Just turn the dang bell off
+opt.signcolumn = "yes" -- always showing the signcolumn
 
 -- Highlight yanked text
 local ag = vim.api.nvim_create_augroup
