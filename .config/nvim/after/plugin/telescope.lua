0d493031ef09957498a61d3ed9508e1e1003a32c
leonasdev <s8952889@gmail.com>
2022-12-09 16:26:22 +0800

    telescope: ignore files bigger than a threshold

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -3,10 +3,26 @@ if not pcall(require, 'telescope') then
 end
 
 local actions = require('telescope.actions')
+local previewers = require('telescope.previewers')
+
+local new_maker = function(filepath, bufnr, opts)
+  opts = opts or {}
+
+  filepath = vim.fn.expand(filepath)
+  vim.loop.fs_stat(filepath, function(_, stat)
+    if not stat then return end
+    if stat.size > 100000 then
+      return
+    else
+      previewers.buffer_previewer_maker(filepath, bufnr, opts)
+    end
+  end)
+end
 
 -- setup
 require('telescope').setup {
   defaults = {
+    buffer_previewer_maker = new_maker,
     mappings = {
       i = {
         ["<esc>"] = actions.close,
@@ -37,7 +53,7 @@ require('telescope').setup {
   }
 }
 
--- require('telescope').load_extension('fzf')
+require('telescope').load_extension('fzf')
 require('telescope').load_extension('file_browser')
 require('telescope').load_extension('live_grep_args')
 
