f1069bb6b56b072dce0580ad098a915ca44cf3b8
Leon <s8952889@gmail.com>
2023-01-31 11:48:36 +0800

    add fish.config & omp theme

---
+++ .config/fish/config.fish
@@ -0,0 +1,22 @@
+set fish_greeting ""
+
+# aliases
+alias ls "ls -p -G"
+alias la "ls -a"
+# alias ll "ls -l"
+# alias lla "ll -A"
+alias ll "exa -l -g --icons"
+alias lla "ll -a"
+alias g git
+command -qv nvim && alias vim nvim
+
+set -gx EDITOR nvim
+
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
+
+if status is-interactive
+# Commands to run in interactive sessions can go here
+    oh-my-posh init fish --config ~/.config/oh-my-posh/sjar.omp.json | source
+end
