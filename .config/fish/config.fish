2a4d62fd3217c54522e0a3cec9dde79ff2b6a98e
leonasdev <s8952889@gmail.com>
2023-01-31 17:24:29 +0800

    update oh-my-posh theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,11 +3,16 @@ set fish_greeting ""
 # aliases
 alias ls "ls -p -G"
 alias la "ls -a"
-# alias ll "ls -l"
-# alias lla "ll -A"
+alias ll "ls -l"
+alias lla "ll -A"
+
+if type -q exa
 alias ll "exa -l -g --icons"
 alias lla "ll -a"
+end
+
 alias g git
+
 command -qv nvim && alias vim nvim
 
 set -gx EDITOR nvim
@@ -18,5 +23,5 @@ set -gx PATH ~/.local/bin $PATH
 
 if status is-interactive
 # Commands to run in interactive sessions can go here
-    oh-my-posh init fish --config ~/.config/oh-my-posh/sjar.omp.json | source
+    oh-my-posh init fish --config ~/.config/oh-my-posh/leonasdev.omp.json| source
 end
