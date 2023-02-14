827ea52368227392ca687bae5d81a5a58315e490
leonasdev <s8952889@gmail.com>
2023-02-14 22:35:29 +0800

    fix wsl condition

--- .config/nvim/lua/leonasdev/options.lua
+++ .config/nvim/lua/leonasdev/options.lua
@@ -40,19 +40,19 @@ opt.backspace = "indent,eol,start"
 -- clipboard
 -- allow you to yank from neovim and C-v to anywhere vice versa
 opt.clipboard:prepend { 'unnamed', 'unnamedplus' }
-if _G.IS_WSL and vim.fn.executable('win32yank-wsl') == 1 then -- you need put win32yank in system32
-    vim.g.clipboard = {
-        name = 'win32yank-wsl',
-        copy = {
-            ["+"] = { "win32yank.exe", "-i", "--crlf" },
-            ["*"] = { "win32yank.exe", "-i", "--crlf" },
-        },
-        paste = {
-            ["+"] = { "win32yank.exe", "-o", "--lf" },
-            ["*"] = { "win32yank.exe", "-o", "--lf" },
-        },
-        cache_enabled = true
-    }
+if _G.IS_WSL and vim.fn.executable('win32yank.exe') == 1 then -- you need put win32yank in system32
+  vim.g.clipboard = {
+    name = 'win32yank-wsl',
+    copy = {
+      ["+"] = { "win32yank.exe", "-i", "--crlf" },
+      ["*"] = { "win32yank.exe", "-i", "--crlf" },
+    },
+    paste = {
+      ["+"] = { "win32yank.exe", "-o", "--lf" },
+      ["*"] = { "win32yank.exe", "-o", "--lf" },
+    },
+    cache_enabled = true
+  }
 end
 
 -- split windows
@@ -90,6 +90,6 @@ vim.api.nvim_create_autocmd("bufEnter", {
   group = vim.api.nvim_create_augroup("FormatOptions", {}),
   pattern = "*",
   callback = function()
-    opt.formatoptions:remove{"r", "o"}
+    opt.formatoptions:remove { "r", "o" }
   end,
 })
