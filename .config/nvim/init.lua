faecd95c46d3dd03ae68b8cd7a670b4412d555fc
leonasdev <s8952889@gmail.com>
2023-05-02 13:57:09 +0800

    Extract globals into a file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,15 +1,5 @@
-local uname = vim.loop.os_uname()
-
-_G.OS = uname.sysname
-_G.IS_MAC = OS == "Darwin"
-_G.IS_LINUX = OS == "Linux"
-_G.IS_WINDOWS = OS:find("Windows") and true or false
-_G.IS_WSL = (function()
-  local output = vim.fn.systemlist("uname -r")
-  local condition1 = IS_LINUX and uname.release:lower():find("microsoft") and true or false
-  local condition2 = not not string.find(output[1] or "", "WSL")
-  return condition1 or condition2
-end)()
+-- Setup globals that I expect to be always available.
+require("leonasdev.globals")
 
 -- Mapping leader key to: " "
 vim.g.mapleader = " "
