1188ad87ace043be9af1b7cdaa07204e640b07a9
leonasdev <s8952889@gmail.com>
2023-05-05 13:16:54 +0800

    Add undo-tree telescope extension

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -99,6 +99,7 @@ return {
       { "gd", lsp_definitions, mode = "n", desc = "LSP Find Definitions" },
       { "gr", lsp_references, mode = "n", desc = "LSP Find References" },
       { "gi", lsp_implementations, mode = "n", desc = "LSP Find Implementations" },
+      { "<leader>u", "<cmd>Telescope undo<cr>", mode = "n", desc = "Undo Tree" },
     },
     config = function()
       require("telescope").setup({
@@ -133,10 +134,21 @@ return {
               },
             },
           },
+          undo = {
+            mappings = {
+              i = {
+                -- ["<cr>"] = require("telescope-undo.actions").yank_additions,
+                -- ["<S-cr>"] = require("telescope-undo.actions").yank_deletions,
+                -- ["<C-cr>"] = require("telescope-undo.actions").restore,
+                ["<cr>"] = require("telescope-undo.actions").restore,
+              },
+            },
+          },
         },
       })
 
       require("telescope").load_extension("fzf")
+      require("telescope").load_extension("undo")
     end,
   },
 
@@ -156,6 +168,12 @@ return {
   -- enable passing arguments to the live_grep of telescope
   {
     "nvim-telescope/telescope-live-grep-args.nvim",
+    lazy = true,
+  },
+
+  -- A telescope extension to view and search your undo tree
+  {
+    "debugloop/telescope-undo.nvim",
     lazy = true,
   },
 
