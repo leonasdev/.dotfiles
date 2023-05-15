57a0ff10b9774782f42aae24a95107c7833be0ed
Leon <s8952889@gmail.com>
2023-05-15 21:25:58 +0800

    fix dap icon

--- .config/nvim/lua/plugins/dap/init.lua
+++ .config/nvim/lua/plugins/dap/init.lua
@@ -62,7 +62,7 @@ return {
     -- stylua: ignore
 		config = function()
 			vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
-			vim.fn.sign_define("DapStopped", { text = " ", texthl = "DiagnosticWarn", linehl = "DapStoppedLine", numhl = "" })
+			vim.fn.sign_define("DapStopped", { text = "󰁕 ", texthl = "DiagnosticWarn", linehl = "DapStoppedLine", numhl = "" })
 			vim.fn.sign_define("DapBreakpoint", { text = " ", texthl = "DiagnosticError", linehl = "", numhl = "" })
 			vim.fn.sign_define("DapBreakpointCondition", { text = " ", texthl = "DiagnosticError", linehl = "", numhl = "" })
 			vim.fn.sign_define("DapBreakpointRejected", { text = " ", texthl = "DiagnosticWarn", linehl = "", numhl = "" })
