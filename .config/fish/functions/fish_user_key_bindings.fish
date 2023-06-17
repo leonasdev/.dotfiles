0e39cccc20dd2512a97f499b8404b4dbdaead7f6
leonasdev <s8952889@gmail.com>
2023-06-17 16:46:21 +0800

    add fish key bindings

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,7 @@
 function fish_user_key_bindings
   bind -e \cd # unbind ctrl-d
-  bind -e \cu # unbind ctrl-d
+  bind -e \cu # unbind ctrl-u
+  bind -e \cp # unbind ctrl-p
   bind \cy accept-autosuggestion
   bind \cb backward-word
   bind \cw forward-word
@@ -8,4 +9,10 @@ function fish_user_key_bindings
   bind \ch backward-kill-path-component # \ch means ctrl+backspace in xterm based terminal-emulator
   bind \cj down-line
   bind \ck up-line
+
+  bind \cp change_directory_with_fzf
+end
+
+function change_directory_with_fzf
+  cd $(fdfind -H -d=2 . ~ | fzf)
 end
