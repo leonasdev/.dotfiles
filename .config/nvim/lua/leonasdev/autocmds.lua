f053a50f17b1ca0394167ba971b4fc281a2ea933
leonasdev <s8952889@gmail.com>
2023-08-23 13:00:47 +0800

    Add new autocmds for instantly hide/show diagnostics when enter/leave insert mode

--- .config/nvim/lua/leonasdev/autocmds.lua
+++ .config/nvim/lua/leonasdev/autocmds.lua
@@ -19,10 +19,28 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 })
 
 -- disable auto comment when insert new line after comment
-vim.api.nvim_create_autocmd("bufEnter", {
+vim.api.nvim_create_autocmd("BufEnter", {
   group = vim.api.nvim_create_augroup("FormatOptions", {}),
   pattern = "*",
   callback = function()
     vim.opt.formatoptions:remove({ "r", "o" })
+  end,
+})
+
+-- instantly hide diagnostic when entering insert mode
+vim.api.nvim_create_autocmd("InsertEnter", {
+  group = vim.api.nvim_create_augroup("HideDiagnostic", {}),
+  pattern = "*",
+  callback = function()
+    vim.diagnostic.hide(nil, 0)
+  end,
+})
+
+-- instantly show diagnostic when leaving insert mode
+vim.api.nvim_create_autocmd("InsertLeave", {
+  group = vim.api.nvim_create_augroup("ShowDiagnostic", {}),
+  pattern = "*",
+  callback = function()
+    vim.diagnostic.show(nil, 0)
   end,
 })
