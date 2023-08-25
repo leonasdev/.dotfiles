1525ef680d886f7ff6061696a0fc9d230b839626
Leon <s8952889@gmail.com>
2023-08-26 00:53:36 +0800

    Add keybinds of fish

--- dot_config/private_fish/functions/fish_user_key_bindings.fish
+++ dot_config/private_fish/functions/fish_user_key_bindings.fish
@@ -3,8 +3,8 @@ function fish_user_key_bindings
   bind -e \cu # unbind ctrl-u
   bind -e \cp # unbind ctrl-p
   bind \cy accept-autosuggestion
-  bind \cb backward-word
-  bind \cw forward-word
+  bind \cb 'cd ..; commandline -f repaint'
+  bind \cw 'prevd; commandline -f repaint'
   bind \cs cancel-commandline
   bind \ch backward-kill-path-component # \ch means ctrl+backspace in xterm based terminal-emulator
   bind \cj down-line
