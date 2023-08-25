f7d3d3247a5b97ba56951e827ec08873fe5b5b5c
Leon <s8952889@gmail.com>
2023-08-26 01:00:30 +0800

    Update .config/fish/functions/fish_user_key_bindings.fish

--- dot_config/private_fish/functions/fish_user_key_bindings.fish
+++ dot_config/private_fish/functions/fish_user_key_bindings.fish
@@ -14,5 +14,5 @@ function fish_user_key_bindings
 end
 
 function change_directory_with_fzf
-  cd $(fdfind --type=directory -H -d=2 . ~ | fzf); commandline -f repaint
+  cd $(fd --type=directory -H -d=2 . ~ | fzf); commandline -f repaint
 end
