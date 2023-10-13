b70e4c349552fc584f853f3c4e6cf32a6656da0b
Leon <s8952889@gmail.com>
2023-10-13 09:29:53 -0700

    Update plugins

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -123,6 +123,7 @@ return {
 
   {
     "lukas-reineke/indent-blankline.nvim",
+    version = "2",
     config = function()
       vim.api.nvim_set_hl(0, "IndentBlanklineContextChar", { link = "IndentBlanklineChar" })
       vim.api.nvim_create_autocmd("ColorScheme", {
