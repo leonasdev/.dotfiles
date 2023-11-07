d15c7b795f6385f13604f3b67d6616a1d151ba67
Leon <s8952889@gmail.com>
2023-11-07 23:11:10 +0800

    Add auto organize imports for go

--- .config/nvim/lua/plugins/formatting/init.lua
+++ .config/nvim/lua/plugins/formatting/init.lua
@@ -1,3 +1,18 @@
+local golang_organize_imports = function()
+  local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding(0))
+  params.context = { only = { "source.organizeImports" } }
+
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+  for _, res in pairs(result or {}) do
+    for _, r in pairs(res.result or {}) do
+      if r.edit then
+        vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding(0))
+      else
+        vim.lsp.buf.execute_command(r.command)
+      end
+    end
+  end
+end
 local formatting_buffer = function()
   local buf = vim.api.nvim_get_current_buf()
   if require("plugins.formatting.autoformat").autoformat == false then
@@ -34,6 +49,17 @@ vim.api.nvim_create_autocmd("LspAttach", {
             return
           end
           formatting_buffer()
+        end,
+      })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*.go",
+        group = vim.api.nvim_create_augroup("LspGolangOrganizeImports." .. bufnr, {}),
+        callback = function()
+          if not require("plugins.formatting.autoformat").autoformat then
+            return
+          end
+          formatting_buffer()
+          golang_organize_imports()
         end,
       })
 
